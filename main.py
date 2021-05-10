2eb97703d9babb404cfcd910f16fc401ab8c2d3d
safisafikul <safisafikuls@gmail.com>
2021-05-11 00:02:32 +0600

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import pyjokes
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-engine.setProperty('voices', voices[1].id)
+engine.setProperty('voice', voices[1].id)
 
 def talk(text):
     engine.say(text)
